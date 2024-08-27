1b6264b8cb62563e924eefa690e07ff4efa42f31
Renegatto <46404781+Renegatto@users.noreply.github.com>
2024-08-27 13:10:12 +0300

    Wrap foreign exports in parentheses
    As purescript-lua requires https://github.com/Unisay/purescript-lua/issues/29

--- src/Effect/Ref.lua
+++ src/Effect/Ref.lua
@@ -1,23 +1,28 @@
-return {
-  _new = function(val) return function() return {value = val} end end,
-  newWithSelf = function(f)
+local function _new(val) return function() return {value = val} end end
+local function newWithSelf(f)
     return function()
       local ref = {value = nil}
       ref.value = f(ref)
       return ref
     end
-  end,
-  read = function(ref) return function() return ref.value end end,
-  modifyImpl = function(f)
-    return function(ref)
-      return function()
-        local t = f(ref.value)
-        ref.value = t.state
-        return t.value
-      end
+end
+local function read(ref) return function() return ref.value end end
+local function modifyImpl(f)
+  return function(ref)
+    return function()
+      local t = f(ref.value)
+      ref.value = t.state
+      return t.value
     end
-  end,
-  write = function(val)
-    return function(ref) return function() ref.value = val end end
   end
+end
+local function write(val)
+  return function(ref) return function() ref.value = val end end
+end
+return {
+  _new = (_new),
+  newWithSelf = (newWithSelf),
+  read = (read),
+  modifyImpl = (modifyImpl),
+  write = (write),
 }
